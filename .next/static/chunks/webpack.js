cb93c84805f1b687c6c9d5d4901b60591f81aea4
Rati <ratigumashvili@gmail.com>
2023-09-22 12:48:28 +0400

    initial commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a09ee435dea7edd1"; }
+/******/ 		__webpack_require__.h = function() { return "19aa124e6a7ab4a2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
