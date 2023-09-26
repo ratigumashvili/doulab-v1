83576b12af63e4f3f4a93ecd59a06d10e6a1857d
Rati <ratigumashvili@gmail.com>
2023-09-26 21:21:54 +0400

    cleanup

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8fbbce7ec7c03752"; }
+/******/ 		__webpack_require__.h = function() { return "f9f2ca1db7d861e6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
