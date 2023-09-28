fd54d4ac18bd4cd2e71d964e3058c2526207741d
Rati <ratigumashvili@gmail.com>
2023-09-28 20:58:03 +0400

    cleanup

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -26,10 +26,6 @@ export const metadata = {
 
 const locales = ["ka", "en"];
 
-export function generateStaticParams() {
-  return locales.map((locale) => ({ locale }));
-}
-
 export default async function LocaleLayout({ children, params: { locale } }) {
   let messages;
   try {
